1abaa34c4236f82ca98a308e01f459c28f2cd8ec
Erich Martinez <erick.martinezsoto@gmail.com>
2021-04-09 01:18:22 -0500

    octavo commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,20 +4,20 @@ const path = require('path')
 
 
 //routes
-router.get('/', (req, res) =>{
+router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../../docs/index.html'));
-} )
+})
 
-router.get('/blog', (req, res) =>{
-  res.sendFile(path.join(__dirname, '../../docs/blog.html'));
-} )
-
-router.get('/servicios', (req, res) =>{
+router.get('/servicios', (req, res) => {
   res.sendFile(path.join(__dirname, '../../docs/services.html'));
-} )
+})
 
-router.get('/nosotros', (req, res) =>{
+router.get('/nosotros', (req, res) => {
   res.sendFile(path.join(__dirname, '../../docs/about.html'));
-} )
+})
+
+router.get('/blog', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../docs/blog.html'));
+})
 
 module.exports = router;
