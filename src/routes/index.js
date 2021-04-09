6b0ee5922fc6b597fb5a42d63842fd6080ad1f20
Erich Martinez <erick.martinezsoto@gmail.com>
2021-04-09 00:31:36 -0500

    sexto commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,19 +5,19 @@ const path = require('path')
 
 //routes
 router.get('/', (req, res) =>{
-  res.sendFile(path.join(__dirname, '../../public/index.html'));
+  res.sendFile(path.join(__dirname, '../../docs/index.html'));
 } )
 
 router.get('/blog', (req, res) =>{
-  res.sendFile(path.join(__dirname, '../../public/blog.html'));
+  res.sendFile(path.join(__dirname, '../../docs/blog.html'));
 } )
 
 router.get('/servicios', (req, res) =>{
-  res.sendFile(path.join(__dirname, '../../public/services.html'));
+  res.sendFile(path.join(__dirname, '../../docs/services.html'));
 } )
 
 router.get('/nosotros', (req, res) =>{
-  res.sendFile(path.join(__dirname, '../../public/about.html'));
+  res.sendFile(path.join(__dirname, '../../docs/about.html'));
 } )
 
 module.exports = router;
