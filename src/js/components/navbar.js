7d7b62cf4a677c550edb898bc054bce2237746fd
Erich Martinez <erick.martinezsoto@gmail.com>
2021-04-10 00:35:28 -0500

    24 commit

--- src/js/components/navbar.js
+++ src/js/components/navbar.js
@@ -8,7 +8,7 @@ const navBar =() => {
     textNavBar.innerHTML += `
   
    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-   <a class="navbar-brand ml-0 ml-md-5" href="/compudev/"><img src=${logoCompudev} alt="" width="120" height="40"></a>
+   <a class="navbar-brand ml-0 ml-md-5" href="/compudevtest/"><img src=${logoCompudev} alt="" width="120" height="40"></a>
    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
      <span class="navbar-toggler-icon"></span>
    </button>
@@ -16,19 +16,19 @@ const navBar =() => {
    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
      <ul class="navbar-nav ml-auto mr-auto mt-2 mt-lg-0">
        <li class="nav-item active">
-         <a class="nav-link py-2" href="/compudev/">Home <span class="sr-only">(current)</span></a>
+         <a class="nav-link py-2" href="/compudevtest/">Home <span class="sr-only">(current)</span></a>
        </li>
        <li class="nav-item">
-         <a class="nav-link py-2" href="/compudev/servicios">Servicios</a>
+         <a class="nav-link py-2" href="/compudevtest/servicios">Servicios</a>
        </li>
        <li class="nav-item">
-         <a class="nav-link py-2" href="/nosotros">Nosotros</a>
+         <a class="nav-link py-2" href="/compudevtest/nosotros">Nosotros</a>
        </li>
        <li class="nav-item">
-         <a class="nav-link" href="/compudev/tienda">Tienda</a>
+         <a class="nav-link" href="/compudevtest/tienda">Tienda</a>
        </li>
        <li class="nav-item">
-         <a class="nav-link py-2" href="/compudev/blog">Blog</a>
+         <a class="nav-link py-2" href="/compudevtest/blog">Blog</a>
        </li>
      </ul>
      <form class="form-inline my-2 my-lg-0 " id="barra-busqueda" style="display: none">
